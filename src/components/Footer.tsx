2f80ba440d8ae2aba8b490e2ff578a958765f243
fariqodri <arfixfari@gmail.com>
2019-01-14 17:52:05 +0700

    Form

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer: React.FunctionComponent<Props> = props => {
     <div {...props} style={{...props.style, textAlign:"center"}}>
       <BigScreen>
         <div>
-          Please fill <a href={"https://gitlab.com/fariqodri/react_portfolio"} target={"_blank"} rel={"noopener noreferrer"}>this form</a> 
+          Please fill <a href={"https://goo.gl/forms/gKS8TTdT0Ue8zldc2"} target={"_blank"} rel={"noopener noreferrer"}>this form</a> 
           <span> </span>if you want to report any issue, suggest a feature, or say something to me.
         </div>
         <div>
@@ -20,7 +20,7 @@ const Footer: React.FunctionComponent<Props> = props => {
       </BigScreen>
       <SmallScreen>
         <div>
-        Please fill <a href={"https://gitlab.com/fariqodri/react_portfolio"} target={"_blank"} rel={"noopener noreferrer"}>this form</a> 
+        Please fill <a href={"https://goo.gl/forms/gKS8TTdT0Ue8zldc2"} target={"_blank"} rel={"noopener noreferrer"}>this form</a> 
           <span> </span>if you want to report any issue, suggest a feature, or say something to me. Some of the designs are inspired by <span> </span> <a href={"https://albertusangga.tech"} target={"_blank"} rel={"noopener noreferrer"}>https://albertusangga.tech</a>
           <span> </span>and Netflix. You can find the source code for this website <span> </span> <a href={"https://gitlab.com/fariqodri/react_portfolio"} target={"_blank"} rel={"noopener noreferrer"}>here</a>
         </div>
